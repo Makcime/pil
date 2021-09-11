82b7d74ad704e9acc2a7424cd07b5188c35e4634
oscar <oscar@pop-os.localdomain>
2021-09-11 21:37:47 +0200

    stop after printing the first solution

--- 2/eight-queen.lua
+++ 2/eight-queen.lua
@@ -28,6 +28,7 @@ end
 function addqueen( a, n )
 	if n > N then 	-- all queens have been placed ?
 		printsollution(a)
+		os.exit()
 	else 	-- try to place n-th queen
 		for c = 1, N do
 			if isplaceok(a, n, c) then
